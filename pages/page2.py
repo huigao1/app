47eff35ea0da6560980ae56900cf485091411b4c
huigao1 <gaohui1@bu.edu>
2025-04-24 19:56:56 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -3,15 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 @st.cache_data(show_spinner=False)
-def load_data(path="esg_cleaned_final.csv"):
-    return pd.read_csv(path)
+def load_data(path="esg_cleaned_final.csv.zip"):
+    return pd.read_csv(path, compression="zip")
 
 df = load_data()
 
 st.title("🏭 Average ESG by Industry Division")
 
 industry_esg = df.groupby('Division')['ESG_Combined_Score'].mean().sort_values()
-
 fig, ax = plt.subplots(figsize=(8, 6))
 industry_esg.plot(kind='barh', ax=ax)
 ax.set_xlabel('Average ESG Combined Score')
