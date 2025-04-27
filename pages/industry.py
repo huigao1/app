5c44ca29d0c8470d737937d51936a569864c6066
huigao1 <gaohui1@bu.edu>
2025-04-27 13:55:23 -0400

    Update industry.py

--- pages/industry.py
+++ pages/industry.py
@@ -1,10 +1,15 @@
-import streamlit as st, matplotlib.pyplot as plt
+import streamlit as st
+import matplotlib.pyplot as plt
 from utils import load_esg_zip
+
 df = load_esg_zip()
 
 st.title("🏭 Industry ESG Overview")
-ind = df.groupby('Division')['ESG_Combined_Score'].mean().sort_values()
 
+# ------------------------------------------------------------------
+# Average ESG per Division (Bar + Top/Bottom tables)
+# ------------------------------------------------------------------
+ind = df.groupby('Division')['ESG_Combined_Score'].mean().sort_values()
 
 c1, c2 = st.columns([2,1])
 with c1:
@@ -17,3 +22,18 @@ with c2:
     st.dataframe(ind.tail(5).to_frame("ESG"))
     st.write("#### 🔻 Bottom 5")
     st.dataframe(ind.head(5).to_frame("ESG"))
+
+# ------------------------------------------------------------------
+# ESG Trend by Division across Years (Line chart)
+# ------------------------------------------------------------------
+st.divider()
+st.subheader("📈 ESG Combined Score Trends by Industry over Time")
+
+trend = df.groupby(['year','Division'])['ESG_Combined_Score'].mean().unstack(fill_value=None)
+fig2, ax2 = plt.subplots(figsize=(12,6))
+trend.plot(ax=ax2)
+ax2.set_title('ESG Combined Score Trends by Industry')
+ax2.set_ylabel('ESG Score')
+ax2.set_xlabel('Year')
+ax2.legend(loc='upper left', bbox_to_anchor=(1.02,1))
+st.pyplot(fig2)
