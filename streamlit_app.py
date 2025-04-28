db074e8dc3164c781c82512ce6e7a07f7b8bbeac
huigao1 <gaohui1@bu.edu>
2025-04-28 01:32:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ App_page_intro  = st.Page("pages/intro.py",              title="Welcome",       
 App_page_eda    = st.Page("pages/eda.py",                title="Exploratory Analysis", icon="🔍")
 App_page_ind    = st.Page("pages/industry.py",           title="Industry ESG",         icon="🏭")
 App_page_trend  = st.Page("pages/trends.py",             title="Time-Series Trends",   icon="⏳")
-App_page_reg    = st.Page("pages/linear_playground.py",  title="Regression Playground",icon="📈")
+App_page_reg    = st.Page("pages/model_choice_playground.py",  title="Regression Playground",icon="📈")
 App_page_cluster= st.Page("pages/clustering.py",         title="Clustering (K-Means)", icon="🧩")
 App_page_detail = st.Page("pages/details.py",            title="Model & Ratios",       icon="📝")
 App_page_predict = st.Page("pages/app.py",            title="Predict by Ticker",     icon="🎯")
