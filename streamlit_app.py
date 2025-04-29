ba74d569e8483e825e9bfc1ea51fe6dded017c8f
Grace0d56 <zehui.wang.grace@outlook.com>
2025-04-29 11:25:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ App_page_predict = st.Page("pages/app.py",            title="Predict by Ticker",
 
 pg = st.navigation({
     "Start":  [App_page_intro],
-    "EDA":    [App_page_eda, App_page_ind, App_page_trend],
+    "Eeplore Data":    [App_page_eda, App_page_ind, App_page_trend],
     "ML":     [App_page_detail, App_page_reg, App_page_predict],
 })
 
